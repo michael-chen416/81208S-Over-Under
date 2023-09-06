c7c67a9ddedc72b6235c54efc74d83500655609f
Canrries Hong <canrrieshong@gmail.com>
2023-09-06 17:10:27 -0400

    fixed catapult conversion error

--- src/subsystems/catapult.cpp
+++ src/subsystems/catapult.cpp
@@ -1,27 +1,36 @@
 #include "subsystems/catapult.hpp"
 
 uint32_t lastPressed = 1500; // needs to be adjusted.
-//catapult down positon: 1480 
-//catapult up position: 8
-void OpCatapult(){
+// catapult down positon: 1480
+// catapult up position: 8
+void OpCatapult()
+{
     okapi::ControllerButton fire(okapi::ControllerDigital::R1);
-    if(potentiometer.get()>1470){   //if not shooting, move the catapult into the correct possition.
-        catapult.moveVoltage(12000); 
-    } else if(fire.changedToPressed()){
+    if (potentiometer.get() > 1470)
+    { // if not shooting, move the catapult into the correct position.
+        catapult.moveVoltage(12000);
+    }
+    else if (fire.changedToPressed())
+    {
         lastPressed = pros::millis();
         catapult.moveVoltage(12000);
-        //pros::delay(); find out time required.
-    } else if(fire.isPressed() && pros::millis - lastPressed > 350){ // fix error
+        // pros::delay(); find out time required.
+    }
+    else if (fire.isPressed() && uint32_t(pros::millis) - lastPressed > uint32_t(350))
+    { // fix error
         catapult.moveVoltage(12000);
-    } else{
+    }
+    else
+    {
         catapult.moveVoltage(0);
     }
 }
 
-void fireCatapult(){ //unused
+void fireCatapult()
+{ // unused
     okapi::ControllerButton fire(okapi::ControllerDigital::R1);
-    if(potentiometer.get() < 1480 && fire.changedToPressed()){ 
+    if (potentiometer.get() < 1480 && fire.changedToPressed())
+    {
         catapult.moveVoltage(12000);
-    } 
-    
+    }
 }
