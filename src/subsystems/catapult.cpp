9d29574501d38c729c79d03ae79736f344674537
Canrries Hong <canrrieshong@gmail.com>
2023-09-09 14:14:24 -0400

    updated catapult potentiometer values

--- src/subsystems/catapult.cpp
+++ src/subsystems/catapult.cpp
@@ -1,20 +1,20 @@
 #include "subsystems/catapult.hpp"
 
 uint32_t lastPressed = 1500;  // needs to be adjusted.
-// catapult down positon: 1450
+// catapult down positon: 1509
 // catapult up position: 8
 
 /* ++++++++++++++ NOTICE ++++++++++++++*/
 /* The catapult slip-gear is not correct, therefore the catapult does not work. */
 void OpCatapult() {
   okapi::ControllerButton fire(okapi::ControllerDigital::R1);
-  if (potentiometer.get() > 1450) {  // if not shooting, move the catapult into the correct position.
+  if (potentiometer.get() > 1500) {  // if not shooting, move the catapult into the correct position.
     catapult.moveVoltage(12000);
   } else if (fire.changedToPressed()) {
     lastPressed = pros::millis();
     catapult.moveVoltage(12000);
     // pros::delay(); find out time required.
-  } else if (fire.isPressed() && uint32_t(pros::millis) - lastPressed > uint32_t(350)) {
+  } else if (fire.isPressed() && uint32_t(pros::millis) - lastPressed > uint32_t(350)) { //jeez this shit is so scuffed I should fix this
     catapult.moveVoltage(12000);
   } else {
     catapult.moveVoltage(0);
