454319b1401f86f4ac9f6417f375889f5d008ddd
Canrries Hong <canrrieshong@gmail.com>
2023-09-29 23:59:47 -0400

    Auton planning + restored catapult file

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,16 @@ uint32_t lastPressed = -800;
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
-void on_center_button() {
+void on_center_button()
+{
 	static bool pressed = false;
 	pressed = !pressed;
-	if (pressed) {
+	if (pressed)
+	{
 		pros::lcd::set_text(2, "I was pressed!");
-	} else {
+	}
+	else
+	{
 		pros::lcd::clear_line(2);
 	}
 }
@@ -22,9 +26,10 @@ void on_center_button() {
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
-void initialize() {
+void initialize()
+{
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "81208S code ");
+	pros::lcd::set_text(1, "81208S code");
 
 	pros::lcd::register_btn1_cb(on_center_button);
 }
@@ -58,7 +63,17 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous()
+{ // Auton code here!
+	// Short path
+	/*'Hold' The triball you start with and rotate at a 50 degree angle. Go forward
+	about 1 diagonal tile, become parallel to the wall and push the triball into
+	the net. Back up and turn a ~70 degree angle and collect the 2nd ball from
+	the barrier. Back up .5 of a tile and rotate ~150 degrees and outtake the
+	ball, it should rest in the goal. Then, rotate 90 degrees, collect the center
+	triball, face the goal, extend wings, and push. This should lead to a 4 ball
+	auton.*/
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -81,18 +96,16 @@ void opcontrol()
 	while (true)
 	{
 		pros::lcd::print(5, "potentiometer value: %f", potentiometer.get());
-		driveChassis();
-		updateIntake();
-		//Catapult code because the catapult file doesn't work unfortunately.
-		//Down Pos: 1480
-		if (potentiometer.get() < 1281) // catapult automatically goes down to the down position. NOTICE: there is quite a big delay, so it is best if you make the potentiometer value around ~200ish lower than the value you want.
-		{ //its so jank???
+		// Catapult code because the catapult file doesn't work unfortunately.
+		// Down Pos: ~1600
+		if (potentiometer.get() < 1281) // Change this value to whatever level you want the catapult to go. There is a slight delay, so it would be best to set the value about ~220 lower than the value you want.
+		{								// Currently the best position for the catapult is around ~1400
 			catapult.moveVoltage(12000);
 		}
 		else if (fire.changedToPressed())
 		{
 			lastPressed = pros::millis();
-			catapult.moveRelative(3000, 12000);
+			catapult.moveRelative(3000, 12000); // Something might be wrong with the values, will do some tweaking.
 		}
 		else if (pros::millis() - lastPressed > 350 && fire.isPressed())
 		{
@@ -102,6 +115,8 @@ void opcontrol()
 		{
 			catapult.moveVoltage(0);
 		}
+		driveChassis();
+		updateIntake();
 		pros::delay(20);
 	}
 }
