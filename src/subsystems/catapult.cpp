83c350894ef7bff5959e283687153f8ac3b13e5b
Canrries Hong <canrrieshong@gmail.com>
2023-10-08 21:09:18 -0400

    Revert "Auton planning + restored catapult file"
    This reverts commit 454319b1401f86f4ac9f6417f375889f5d008ddd.

--- src/subsystems/catapult.cpp
+++ src/subsystems/catapult.cpp
@@ -1,26 +1,8 @@
 #include "subsystems/catapult.hpp"
-uint32_t lastPressed = -800;
+
 /*NOTICE*/
-
-//The actual code is in main, I just left this here so that in the unlikely case that this file works again, I don't have to go through the hassle of bringing everything back
-
+//CODE IS IN MAIN!!!
+//GO THERE IF YOU WANT TO FIX THE CATA CODE!!!
 void OpCatapult() {
-    okapi::ControllerButton fire(okapi::ControllerDigital::R1);
-    if (potentiometer.get() < 1281) 
-		{ 
-			catapult.moveVoltage(12000);
-		}
-		else if (fire.changedToPressed())
-		{
-			lastPressed = pros::millis();
-			catapult.moveRelative(3000, 12000);
-		}
-		else if (pros::millis() - lastPressed > 350 && fire.isPressed())
-		{
-			catapult.moveVoltage(12000);
-		}
-		else
-		{
-			catapult.moveVoltage(0);
-		}
+
 }
